ffc5ea4046fd81376d32ca71d1602a3db8a23436
Dexter <raultasayco54321@gmail.com>
2026-06-19 09:19:35 -0500

    fix connection swagger

--- obj/Debug/net9.0/FinTrackBack.AssemblyInfo.cs
+++ obj/Debug/net9.0/FinTrackBack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinTrackBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7ee772d0d0659df35cdd882bf559613ea43d3ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bd433fe56c2bb9229b8ae33c54a61e898ae7ac8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinTrackBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinTrackBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
